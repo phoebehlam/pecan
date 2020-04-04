44ae68db4c146368015eb8a545b3643a9053f4fd
phoebe lam <phoebelam@Phoebes-MacBook-Pro-2018.local>
2020-04-04 14:38:50 -0500

    aesthetics for startup msg

--- R/sunrise.R
+++ R/sunrise.R
@@ -1,4 +1,4 @@
 #sunrise. at every start-up.
 .onAttach <- function(libname,pkgname) {
-  packageStartupMessage('\np  e  c  a  n  \nsomeone once roasted pecans \nthey became essential\ntho oblivious before encounter\njust like someone\njust like these functions\n')
+  packageStartupMessage('\np  e  c  a  n\nsomeone once roasted pecans \ntho oblivious before\nthey became essential\njust like someone\njust like these functions\n')
 }
