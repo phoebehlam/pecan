82e2a85c64484a4e6d0ed637b4764e3f209ca0ca
phoebe lam <phoebelam@Phoebes-MacBook-Pro-2018.local>
2020-04-03 21:53:04 -0500

    Initial commit

---
+++ R/winsor.R
@@ -0,0 +1,33 @@
+#' winsorize function
+#'
+#' winsorize to next lowest/highest value based on user input sd from the mean
+#' @param x vector of data
+#' @param sd the number of standard deviations from mean as the criterion for winsorizing
+#' @export
+winsor <- function (x, sd) {
+  z = scale(x)
+
+  posout<-which(z >= sd)
+  negout<-which(z <= -sd)
+
+  xna=x
+  xna[posout] <- "pos"
+  xna[negout] <- "neg"
+
+  high <- max( as.numeric(xna), na.rm=T)
+  low <- min( as.numeric(xna), na.rm=T)
+
+  xwin1 <- replace (xna, xna == "pos", high)
+  xwin2 <- replace (xwin1, xwin1 == "neg", low)
+
+  return(xwin2)
+
+}
+
+.onAttach <- function(libname,pkgname) {
+  packageStartupMessage('someone once roasted pecans for me n since then
+                        \nsomething tells me that pecans would become essential
+                        \nan essential previously oblivious to me
+                        \njust like her
+                        \njust like these functions')
+  }
