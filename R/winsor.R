04beb00a20769b96e46296ad7a9de769650c4e8a
chialam18 <phoebelam@u.nortwestern.edu>
2020-04-11 12:09:05 -0500

    winsor numeric and suppress warnings

--- R/winsor.R
+++ R/winsor.R
@@ -6,20 +6,22 @@
 #' @export
 winsor <- function (x, sd) {
   z = scale(x)
-  
+
   posout<-which(z >= sd)
   negout<-which(z <= -sd)
-  
+
   xna=x
   xna[posout] <- "pos"
   xna[negout] <- "neg"
-  
-  high <- max( as.numeric(xna), na.rm=T)
-  low <- min( as.numeric(xna), na.rm=T)
-  
+
+  high <- suppressWarnings(max( as.numeric(xna), na.rm=T))
+  low <- suppressWarnings(min( as.numeric(xna), na.rm=T))
+
   xwin1 <- replace (xna, xna == "pos", high)
   xwin2 <- replace (xwin1, xwin1 == "neg", low)
-  
+
+  xwin2<-as.numeric(as.character(xwin2))
+
   return(xwin2)
-  
+
 }
