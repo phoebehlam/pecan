1cde97a803960a04951924ff65423d7cf729962d
chialam18 <phoebelam@u.nortwestern.edu>
2020-06-30 11:16:52 -0500

    mergethem

--- R/mergethem.R
+++ R/mergethem.R
@@ -1,17 +1,19 @@
-#' merge multiple datasets together
-#' 
-#' @param ... list of dataframes. the matching column must be first variable
-#' 
-#' @example
-#' Reduce(mergethem, list(a, b, c, d, e))
-#' 
+#' mergethem
+#'
+#' merge multiple dfs together, identifier must be first column, doesn't need to be same name
+#' @param ... dataframes separated by comma
 #' @export
-mergethem <- function(..., id){
-  df1 = list(...)[[1]]
-  df2 = list(...)[[2]]
-  col1 = colnames(df1)[1]
-  col2 = colnames(df2)[1]
-  xxx=merge(..., by.x = col1, by.y = col2, all = TRUE)
-  return(xxx)
+mergethem <- function (...){
+  
+  func <- function(...){
+    df1 = list(...)[[1]]
+    df2 = list(...)[[2]]
+    col1 = colnames(df1)[1]
+    col2 = colnames(df2)[1]
+    xxx=merge(..., by.x = col1, by.y = col2, all = TRUE)
+    return(xxx)
+  }
+  
+  Reduce(func, list(...)) 
+  
 }
-
