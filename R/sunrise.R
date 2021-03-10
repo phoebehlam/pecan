76553a31e74da08ab91c856875338c32d262f12e
chialam18 <phoebelam@u.nortwestern.edu>
2021-03-10 10:30:33 -0600

    pecan updates

--- R/sunrise.R
+++ R/sunrise.R
@@ -1,6 +1,6 @@
 #sunrise. at every start-up.
 .onAttach <- function(libname,pkgname) {
-  packageStartupMessage('\np  e  c  a  n\nsomeone once roasted pecans \ntho oblivious before\nthey became essential\njust like someone\njust like these functions\n')
+  packageStartupMessage('\np  e  c  a  n\n')
 }
 
 #set the kitchen as directory
---
+++ R/why.R
@@ -0,0 +1,6 @@
+#' why
+#'
+#' @export
+why <- function(){
+  print(cat("\np  e  c  a  n\nsomeone once roasted pecans \ntho oblivious before\nthey became essential\njust like someone\njust like these functions"))
+}
